366c9fe6937ede866a26f12e80212a80c7d4bed1
Evandro oliveira <vandoaparecido@hotmail.com>
2020-08-30 22:23:16 -0300

    Criando rota privada

---
+++ src/auth.js
@@ -0,0 +1,1 @@
+export const isAlthenticated = () => true;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import{isAlthenticated} from './auth';
 
 import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route 
+        {...rest}
+         render={props =>
+        isAlthenticated() ? (
+            <Component {...props} />
+        ):(
+            <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
+
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={() => <h1>Hello world</h1>} />
+            <PrivateRoute path="/app" component={() => <h1>Você está logado</h1>} />
         </Switch>
     </BrowserRouter>
 );
